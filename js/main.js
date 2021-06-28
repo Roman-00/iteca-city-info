4d2267a56bcc7472fc51180ceb2e4de75fc5f7f9
Roman <rvasin00@gmail.com>
2021-06-28 16:55:04 +0600

    feat: add a style guide

--- js/main.js
+++ js/main.js
@@ -1,157 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-    
-    /*
-        * Получаем блоки на сайте, для создания, и вложения ифнормации
-        ! Ни в коем случае не удалять и не изменять название переменных 
-    */
-    const tabsContainer = document.querySelector('.tabscontainer'),
-        tabheaderItems = document.querySelector('.tabheader__items');
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict';
 
-    /*
-        * Создаем тестовый массив данных для создания функционала 
-    */
+	/*
+		* Получаем елементы с html, и записываем их в переменные
+	*/
 
-    const tabsDataInfo = [
-        {
-            id: "Qatar",
-            title: "Qatar",
-            visa: true,
-            permission: true,
-            list: [
-            "Apply to kazakhstan embassy with Permission from mvk", 
-            "Pcr test 72 hours", 
-            "Ashyq app download Before flight"
-            ]
-        },
-        {
-            id: "Almaty",
-            title: "Almaty",
-            visa: true,
-            permission: false,
-            list: [
-            "Pcr test 72 hours", 
-            "Ashyq app download Before flight"
-            ]
-        },
-        {
-            id: "Шимкент",
-            title: "Шимкент",
-            visa: false,
-            permission: true,
-            list: [ 
-            "Ashyq app download Before flight"
-            ]
-        }
-    ];
+	const tabsContainer = document.querySelector('.tabscontainer'),
+		tabHeader = document.querySelector('.tabheader'),
+		tabHeaderItems = document.querySelector('.tabheader'),
+		tabContentContainer = document.querySelector('.tabheader');
 
-    /*
-        * Создаем функцию которая генерирует нам список городов. 
-    */
-    const createCityList = ( { id, title } ) => {
+	/*
+		* Создаем функцию getData которая получает данные с файла city.json
+		! Ни в коем случае не менять параметры, и не удалять функцию
+	*/
 
-        /*
-            * Создаем переменную "cityList" и ложим в него верстку наших городов
-            * С помощью "insertAdjacentHTML" пушим верстку в контейнер "tabheaderItems"
-        */
+	const getData = async (url) => {
 
-        const cityList = `
-            <div class="tabheader__item tabheader__item_active" data_id="${id}">${title}</div>
-            <!-- /.tabheader__item -->
-        `;
+		const response = await fetch(url);
 
-        tabheaderItems.insertAdjacentHTML('beforeend', cityList);
+		if(!response.ok) {
+			throw new Error(`Ошибка по адресу ${utl}, статус ошибки ${response.status} !`);
+		}
 
-    };
+		return await response.json();
 
-    const createCityListInfo = ( { title, visa, permission, list } ) => {
+	};
 
-        let textVisa;
-        let textPermission;
+	/*
+		* Сохдаем функцию "createCityList", которая нам сгенерирует список городов
+	*/
 
-        if( visa == true ) {
-            textVisa = "Visa Required";
-        } else {
-            textVisa = "";
-        }
+	const createCityList = ({ id, title }) => {
 
-        if (permission == true) {
-            textPermission = "Permission from mvk (Ask Iteca)";
-        } else {
-            textPermission = "";
-        }
+		const cityList = `
+			<div class="tabheader__item tabheader__item_active" data_id="${id}">${title}</div>
+			<!-- /.tabheader__item -->
+		`;
 
-        const cityInfo = `
-            <div class="tabcontent">
-                <h2 class="tabscontent__city--name">
-                    ${title}
-                </h2>
-                <div class="tabscontent__city--info">
-                    <div class="tabscontent__city--visa">
-                        ${textVisa}
-                    </div>
-                    <div class="tabscontent__city--permission">
-                        ${textPermission}
-                    </div>
-                </div>
-                <ul class="tabscontent__city--list">
-                    ${list.map(item => `
-                        <li>${item}</li>
-                    `)}
-                </ul>
-            </div>
-            <!-- /.tabcontent -->
-        `;
+		tabHeaderItems.insertAdjacentHTML('beforeend', cityList);
+	};
 
-        tabsContainer.insertAdjacentHTML('beforeend', cityInfo);
+	/*
+		* Создаем функцию "createCityInfo" которая нам будет генерировать контент для городов
+	*/
 
-    };
+	const createCityInfo = () => {
 
-    tabsDataInfo.forEach(createCityList);
-    tabsDataInfo.forEach(createCityListInfo);
+		const cityInfo = ``;
 
-    const tabHeader = document.querySelector('.tabheader'), 
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabs = document.querySelectorAll('.tabheader__item');
-
-    
-    const hideTabContent = () => {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    };
-    
-    const showTabContent = ( i = 0 ) => {
-
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-
-    };
-
-    hideTabContent();
-    showTabContent();
-
-    tabHeader.addEventListener('click', (event) => {
-
-        const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-
-    });
+	};
 
 
-    
+	const init = () => {
+
+		/*
+			* Получаем список городов с файла city.json
+		*/
+
+		getData('../city.json').then((data) => {
+			console.log('data', data);
+			data.forEach(createCityList);
+		});
+
+	};
+
+	init();
+
 });
